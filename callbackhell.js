7b5a097c68e30ef8182ac07f9159122f29ab0fc8
Sai Swaroop <saiswaroop874@gmail.com>
2023-01-21 12:16:44 +0300

    init

---
+++ callbackhell.js
@@ -0,0 +1,14 @@
+// callbackhell is one callback inside another call back inside another call back........ 
+// callbacks are super powerfull way of handiling async operations in javascript 
+// issues with callbacks
+// 
+// 1) callback hell (code becomes unmaintainable) 2) inversion of control 
+ console.log("hello");
+
+setTimeout(function(){
+      console.log("Swaroop");
+}, 3000) 
+
+ console.log("How are you");
+
+ 
