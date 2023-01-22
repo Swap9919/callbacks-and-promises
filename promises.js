242eb98989f9c4a2e9c2435d319dae6465daae22
Sai Swaroop <saiswaroop874@gmail.com>
2023-01-22 13:20:39 +0300

    init

--- promises.js
+++ promises.js
@@ -2,7 +2,7 @@
 // the API will return us a promise(nothing but an empty object) and the program will go on executing and later on at certain point promise fills the empty object with our desired value at what ever async time it takes
 
 
-// promise is an object which represents the eventual completion of an async operation
+// promise is an object which represents the eventual completion of an async operation.
 
 const cart = [ "shoes", "pants", "watches"];
 
